743e74a43904e0883ce354a29abd99afcc7fd81d
fraserprice <fp914@ic.ac.uk>
2019-02-16 21:58:14 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+This mini project is a look at the secure design of smart contracts in Solidity, the Ethereum language. 
+
+A full description of the design decisions for this application is available [here](https://docs.google.com/document/d/1erZGfSHhakVbQmS8XeoDqjAqcbw8v3I4N7GGMpi36Fo/edit?usp=sharing).
+
---
+++ RockPaperScissorsSecure.sol
@@ -0,0 +1,168 @@
+pragma solidity 0.5.0;
+
+contract RockPaperScissors {
+    uint8 constant STAKE = 10;
+    uint16 constant GAME_TIMEOUT = 300;
+    bytes32 constant DEFAULT_HASH = "";
+    
+    uint64 gameCounter = 0;
+    address payable waitingPlayer;
+    mapping (uint => Game) games;
+    mapping (uint8 => mapping (uint8 => uint8)) winnerEncoding;
+
+    struct Game {
+        address payable p1;
+        address payable p2;
+        uint8 p1Move;
+        uint8 p2Move;
+        bytes32 p1MoveHash;
+        bytes32 p2MoveHash;
+        bool gameInProgress;
+        uint timeout;
+    }
+
+    event GameStart(uint gameID, address p1, address p2);
+    event CommitRequest(uint gameID, address p1, address p2);
+    event GameEnd(uint gameID, uint8 winner);
+    event GameTimeout(uint gameID);
+
+    modifier sentPayIn {
+        require(msg.value == STAKE);
+        _;
+    }
+
+    modifier gameInProgress(uint gameID) {
+        checkGameState(gameID);
+        _;
+    }
+    
+    modifier playerInGame(uint gameID) {
+        Game memory game = games[gameID];
+        require(msg.sender == game.p1 || msg.sender == game.p2);
+        _;
+    }
+    
+    modifier playerNotMoved(uint gameID) {
+        Game memory game = games[gameID];
+        if (game.p1 == msg.sender) {
+            require(game.p1MoveHash == DEFAULT_HASH);
+        }
+        if (game.p2 == msg.sender) {
+            require(game.p2MoveHash == DEFAULT_HASH);
+        }
+        _;
+    }
+    
+    modifier bothPlayersMoved(uint gameID) {
+        Game memory game = games[gameID];
+        require(game.p1MoveHash != DEFAULT_HASH && game.p2MoveHash != DEFAULT_HASH);
+        _;
+    }
+
+    constructor() public {
+        // Initialize winning mapping to avoid work when game played
+        // Moves-   0: Rock, 1: Paper, 2: Scissors
+        // Results- 0: Draw, 1: P1,    2: P2
+        winnerEncoding[0][0] = 0;
+        winnerEncoding[1][1] = 0;
+        winnerEncoding[2][2] = 0;
+        winnerEncoding[0][2] = 1;
+        winnerEncoding[1][0] = 1;
+        winnerEncoding[2][1] = 1;
+        winnerEncoding[2][0] = 2;
+        winnerEncoding[0][1] = 2;
+        winnerEncoding[1][2] = 2;
+    }
+
+
+    function joinGame() public {
+        if (waitingPlayer != address(0) && waitingPlayer != msg.sender) {
+            games[gameCounter] = Game({p1: waitingPlayer, p2: msg.sender, 
+                                       p1Move: 3, p2Move: 3, 
+                                       p1MoveHash: DEFAULT_HASH, p2MoveHash: DEFAULT_HASH, 
+                                       gameInProgress: true, timeout: now + GAME_TIMEOUT});
+            emit GameStart(gameCounter, waitingPlayer, msg.sender);
+            waitingPlayer = address(0);
+            gameCounter++;
+        } else {
+            waitingPlayer = msg.sender;
+        }
+    }
+
+    function makeMove(uint gameID, bytes32 moveHash) payable public
+        sentPayIn
+        gameInProgress(gameID)
+        playerInGame(gameID)
+        playerNotMoved(gameID)
+    {
+        Game storage game = games[gameID];
+        if (msg.sender == game.p1) {
+            game.p1MoveHash = moveHash;
+        } else if (msg.sender == game.p2) {
+            game.p2MoveHash = moveHash;
+        }
+        if (game.p1MoveHash != DEFAULT_HASH && game.p2MoveHash != DEFAULT_HASH) {
+            game.timeout = now + GAME_TIMEOUT;
+            emit CommitRequest(gameID, game.p1, game.p2);
+        }
+    }
+
+    function commit(uint gameID, uint8 move, string memory password) public 
+        gameInProgress(gameID)
+        playerInGame(gameID)
+        bothPlayersMoved(gameID)
+    {
+        Game storage game = games[gameID];
+        uint8 winner = 3;
+        if (msg.sender == game.p1 && keccak256(abi.encodePacked(gameID, move, password)) == game.p1MoveHash) {
+            game.p1Move = move;
+        } else if (msg.sender == game.p2 && keccak256(abi.encodePacked(gameID, move, password)) == game.p2MoveHash) {
+            game.p2Move = move;
+        } else {
+            revert();
+        }
+        
+        if (game.p1Move != 3 && game.p2Move != 3) {
+            winner = winnerEncoding[game.p1Move][game.p2Move];
+            emit GameEnd(gameID, winner);
+            
+            if (winner == 0) {
+                game.p1.transfer(STAKE);
+                game.p2.transfer(STAKE);
+            } else if (winner == 1) {
+                game.p1.transfer(STAKE * 2);
+            } else {
+                game.p2.transfer(STAKE * 2);
+            }
+            game.gameInProgress = false;
+        }
+    }
+    
+        
+    function checkGameState(uint gameID) public {
+        Game storage game = games[gameID];
+        require(game.gameInProgress);
+        if (now > game.timeout) {
+            if (game.p1MoveHash != DEFAULT_HASH && game.p2MoveHash != DEFAULT_HASH) {
+                if (game.p1Move == 3 && game.p2Move != 3) {
+                    game.p2.transfer(STAKE * 2);
+                } else if (game.p2Move == 3 && game.p1Move != 3) {
+                    game.p1.transfer(STAKE * 2);
+                }
+            }
+            if (game.p1Move == 3 && game.p2Move == 3) {
+                if (game.p1MoveHash != DEFAULT_HASH) {
+                    game.p1.transfer(STAKE);
+                }
+                if (game.p2MoveHash != DEFAULT_HASH) {
+                    game.p2.transfer(STAKE);
+                }
+            }
+            
+            game.gameInProgress = false;
+            emit GameTimeout(gameID);
+            revert();
+        }
+    }
+}
+
